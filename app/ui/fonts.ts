1a89a5c200f6c01d519cce2d3969c7eb3a0c43b3
Rémy THELIGENE <theligener@yahoo.fr>
2025-01-25 23:29:56 -0500

    Correction du fonts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,6 +1,9 @@
 import { Kanit, Lusitana } from 'next/font/google';
  
-export const inter = Kanit({ weight:['400']});
+export const inter = Kanit({ 
+    weight:['400'],
+    subsets: ['latin'],
+});
 
 export const lusitana = Lusitana({
     weight: ['400', '700'],
